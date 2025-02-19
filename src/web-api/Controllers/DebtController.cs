7d6fb1c92a907986b134b05c71e4feaf69710962
marekvonrogall <marekvonrogall@gmx.ch>
2025-02-19 16:55:15 +0100

    Add basic web api structure

---
+++ src/web-api/Controllers/DebtController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DebtController : ControllerBase
+{
+    [HttpPost(Name = "update")]
+    public async Task<IActionResult> ModifyDebt()
+    {
+        //Modify debt in database
+        return Ok(new
+        {
+            "Debt modified"
+        });
+    }
+
+    [HttpGet(Name = "read")]
+    public async Task<IActionResult> ReadDebt()
+    {
+        // Read debt from database
+        return Ok(new
+        {
+            "debt: ????"
+        });
+    }
+}
---
+++ src/web-api/Program.cs
@@ -0,0 +1,38 @@
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.Listen(IPAddress.Any, 5001);
+});
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
