7b26b63f5a55741b67ff4672c37f266aa93cee39
marekvonrogall <marekvonrogall@gmx.ch>
2025-02-21 13:58:10 +0100

    Add database update and read functionality to DebtController

--- src/web-api/Controllers/DebtController.cs
+++ src/web-api/Controllers/DebtController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace web_api.Controllers;
 
@@ -6,23 +7,58 @@ namespace web_api.Controllers;
 [Route("[controller]")]
 public class DebtController : ControllerBase
 {
-    [HttpPost("update")]
-    public async Task<IActionResult> ModifyDebt()
+    private const string ConnectionString = "Server=lb223.vrmarek.me,1433; Database=lb223; User Id=sa; Password={0};";
+    
+    [HttpGet("ping")]
+    public async Task<IActionResult> Ping()
     {
-        //Modify debt in database
         return Ok(new
         {
-            message = "Debt modified"
+            message = "OK"
         });
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> ModifyDebt([FromBody] decimal newDebt)
+    {
+        if (newDebt < 0)
+        {
+            return BadRequest(new { message = "Invalid input" });
+        }
+
+        using (SqlConnection conn = new SqlConnection(ConnectionString))
+        {
+            await conn.OpenAsync();
+            string query = "UPDATE Debts SET debt = @NewDebt WHERE (SELECT MIN(debt) FROM Debts) IS NOT NULL";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@NewDebt", newDebt);
+                int rowsAffected = await cmd.ExecuteNonQueryAsync();
+                if (rowsAffected > 0)
+                {
+                    return Ok(new { message = "Debt modified" });
+                }
+                return NotFound(new { message = "Debt not found" });
+            }
+        }
     }
 
     [HttpGet("read")]
     public async Task<IActionResult> ReadDebt()
     {
-        // Read debt from database
-        return Ok(new
+        using (SqlConnection conn = new SqlConnection(ConnectionString))
         {
-            debt = "placeholder"
-        });
+            await conn.OpenAsync();
+            string query = "SELECT TOP 1 debt FROM Debts";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                object result = await cmd.ExecuteScalarAsync();
+                if (result != null)
+                {
+                    return Ok(new { debt = result });
+                }
+                return NotFound(new { message = "Debt not found" });
+            }
+        }
     }
 }
